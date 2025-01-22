d7f8a0f2355b41fd35bf2b46c113c4639bdb8c42
Karl Vestin <rixw@me.com>
2025-01-21 20:54:52 -0500

    logic development

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const allBtns = [...document.querySelectorAll(".calc__btn")];
 const btnNumbers = [...document.querySelectorAll(".btn-num")];
 const btnOperators = [...document.querySelectorAll(".btn-op")];
-const allClearBtn = document.querySelector(".all-clear")
+const allClearBtn = document.querySelector(".clear-ac")
+const oneClearBtn = document.querySelector(".clear-c")
 // Displays
 const calculationDisplayContainer = document.querySelector(".calc__display__calculation-container");
 const calculationDisplayTotalText = document.querySelector(".calc__display__calculation-total-text");
@@ -17,109 +18,110 @@ let equalSignWidth = calculationDisplayEqualSign.getBoundingClientRect().width;
 calculationDisplayContainer.style.paddingLeft = equalSignWidth + "px";
 
 let calculationArray = [0, 0];
-
-let totalTypedNow;
-let typingTotalNumber;
-let displayFormula;
-let minusPlus = false;
-let activeOperator = add;
-let baseNumber = calculationArray[calculationArray.length - 2];
-let newNumber = calculationArray[calculationArray.length - 1];
+let individualDigits = [];
+let decimal = false;
+let numberBuilder;
 let liveCalcResult;
-let calcDisplayAllValues = []
-let numberLocked = false;
-// let newNumber = 0;
+let activeOperator = add;
+let tempResult;
+let storedResults = [0];
+let previousCalcDisplayText;
+
 
 function operate(num1, num2, operator) {
-    return operator(num1, num2)
+    return parseFloat(operator(num1, num2))
 }
 
 allBtns.forEach(btn => {
 
     btn.addEventListener("click", (e) => {
         let tempCalc;
-        btn.classList.contains("op-all-clear") && clearCalc();
+        btn.classList.contains("clear-ac") && clearCalc();
+        btn.classList.contains("clear-c") && clearOneCalc();
         btn.classList.contains("btn-num") && regNum(btn);
-        btn.classList.contains("btn-op") && opNum(btn);
         btn.classList.contains("btn-plus-minus") && plusMinus(btn);
         btn.classList.contains("btn-plus-minus") ? minusPlus = true : minusPlus = false;
+        
         console.log(calculationArray);
+
+        btn.classList.contains("btn-op") && opNum(btn, numberBuilder);
+
+        numberBuilder = parseFloat(individualDigits.reduce((acc, number) => acc * 1 + number, 0));
+
+        // liveCalcResult = operate(calculationArray.at(-2), calculationArray.at(-1), activeOperator)
+        // console.log(liveCalcResult);
+        tempResult = operate(calculationArray.at(-1), numberBuilder, activeOperator)
+        console.log(parseFloat(tempResult));
+        resultContainer.textContent = tempResult.toFixed(2);
+
+
     })
 })
 
-function opNum(btn) {
-    // Get operator symbol from calc button
-    let operatorPick = btn.textContent;
-    // Add operator symbol to calc array
-    calculationArray.push(liveCalcResult);
-    // Clear previous total number
-    totalTypedNow = "";
-    // Add operator symbol to calc display
-    calculationDisplayText.textContent += operatorPick;
+function clearOneCalc() {
+    individualDigits.pop()
+    calculationDisplayText.textContent = previousCalcDisplayText;
+    // resultContainer.textContent = storedResults.at(-1)
+    // console.log(calculationArray);
+    // console.log(poppedDigits);
+}
 
-    if (operatorPick === "-") {
+function opNum(btn, numberBuilder) {
+    individualDigits = []
+    storedResults.push(tempResult)
+    calculationArray.push(tempResult);
+    tempResult = [];
+    numberBuilder = []
+    console.log(calculationArray);
+
+    calculationDisplayText.textContent += btn.textContent;
+
+
+    // Get operator symbol from calc button
+
+    if (btn.textContent === "-") {
         activeOperator = subtract
     } else {
         activeOperator = add;
     }
-
 }
 function plusMinus() {
     console.log(totalTypedNow);
 
-    // totalTypedNow = parseInt(`-${totalTypedNow}`)
-    // calcLiveResult(parseInt(totalTypedNow))
-    // console.log(currentNumbers);
-    // plusMinusSign.classList.toggle("show");
-    // console.log(calculationTotalText.textContent);
 }
 function regNum(btn) {
+    let digit = btn.textContent
 
-    // Get the pressed numbers text content
-    let numberPick = btn.textContent;
-    // If current number is empty set to button text content, otherwise add number to text content
-    if (totalTypedNow === undefined) {
-        totalTypedNow = numberPick;
+    previousCalcDisplayText = calculationDisplayText.textContent;
+
+    
+    if (digit === ".") {
+        console.log("its a dott");
+        calculationDisplayText.textContent = calculationDisplayText.textContent += digit;
+        decimal = true;
+    } else if (decimal === true) {
+        let decimalDigit = `.${digit}`;
+        individualDigits.push(decimalDigit)
+        calculationDisplayText.textContent = calculationDisplayText.textContent += digit;
+        decimal = false;
     } else {
-        totalTypedNow += numberPick;
-    }
-
-    if (minusPlus) {
-        totalTypedNow = `-${totalTypedNow}`
-    }
-    // Add new numbers to calculation display
-    // typingTotalNumber = numberPick;
-    console.log(calcDisplayAllValues);
-
-
-    totalTypedNow = parseInt(totalTypedNow)
-    if (!numberLocked) {
-        calcDisplayAllValues[calcDisplayAllValues.length -1] = totalTypedNow;
-    } else {
-        calcDisplayAllValues.push(totalTypedNow)
+        individualDigits.push(digit)
+        calculationDisplayText.textContent = calculationDisplayText.textContent += digit;
 
     }
-    console.log(calcDisplayAllValues);
+    console.log(individualDigits);
+    // individualDigits.push(btn)
+    // console.log(thisNumber);
+    // thisNumber = 0;
 
 
-
-    displayCalculation(totalTypedNow)
-    calculationDisplayText.textContent = totalTypedNow;
-
-
-    calcLiveResult(parseInt(totalTypedNow))
-    // console.log(baseNumber, newNumber);
 }
 
 function calcLiveResult(totalTypedNow) {
-    liveCalcResult = operate(calculationArray.at(-1), totalTypedNow, activeOperator)
-    resultContainer.textContent = liveCalcResult;
 
-    // console.log(totalTypedNow + calculationArray.at(-1));
 }
 
 function displayCalculation(totalTypedNow) {
-    calcDisplayAllValues[calcDisplayAllValues.length - 1] = totalTypedNow;
 
 }
 
@@ -134,13 +136,5 @@ function divide(a, b) {return a / b;}
 // console.log(add(2,-8));
 
 function clearCalc() {
-    calculationDisplayText.textContent = "";
-    resultContainer.textContent = "";
-    totalTypedNow = "";
-    plusMinusSign.classList.remove("show");
-    calculationArray = [0, 0];
-    baseNumber = calculationArray[calculationArray.length - 2];
-    newNumber = calculationArray[calculationArray.length - 1];
-    minusPlus = false;
-    activeOperator = add;
+
 }
