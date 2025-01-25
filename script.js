42fc953960455dffd8194c44ffe31467e444dd54
Karl Vestin <rixw@me.com>
2025-01-25 07:23:44 -0500

    stuck on logic, will try simple version

--- script.js
+++ script.js
@@ -17,15 +17,28 @@ let equalSignWidth = calculationDisplayEqualSign.getBoundingClientRect().width;
 // console.log(equalSignWidth);
 calculationDisplayContainer.style.paddingLeft = equalSignWidth + "px";
 
-let calculationArray = [0, 0];
+let calcResultsArray = [];
+let calcWholeNumbers = [];
+let operatorRun = [];
 let individualDigits = [];
 let decimal = false;
 let numberBuilder;
 let liveCalcResult;
 let activeOperator = add;
 let tempResult;
-let storedResults = [0];
 let previousCalcDisplayText;
+let previousCalcDisplayTextBeforeDot;
+let lastOperatorSymbol;
+const allOperatorSymbols = ["+", "-", "x", "/", "%"];
+let joinedDigits;
+let calcDisplayArray = []
+let joinedCalcDisplayArray;
+let calcSumsArray = []
+let negDigit = false;
+let minusSign = "";
+
+let calcWholeNumbersLength;
+let calcResultArrayLength;
 
 
 function operate(num1, num2, operator) {
@@ -41,80 +54,95 @@ allBtns.forEach(btn => {
         btn.classList.contains("btn-num") && regNum(btn);
         btn.classList.contains("btn-plus-minus") && plusMinus(btn);
         btn.classList.contains("btn-plus-minus") ? minusPlus = true : minusPlus = false;
-        
-        console.log(calculationArray);
+
 
         btn.classList.contains("btn-op") && opNum(btn, numberBuilder);
 
         numberBuilder = parseFloat(individualDigits.reduce((acc, number) => acc * 1 + number, 0));
 
-        // liveCalcResult = operate(calculationArray.at(-2), calculationArray.at(-1), activeOperator)
-        // console.log(liveCalcResult);
-        tempResult = operate(calculationArray.at(-1), numberBuilder, activeOperator)
-        console.log(parseFloat(tempResult));
-        resultContainer.textContent = tempResult.toFixed(2);
+        
+        joinedCalcDisplayArray = calcDisplayArray.join("")
 
+        calculationDisplayText.textContent = joinedCalcDisplayArray;
 
     })
 })
 
 function clearOneCalc() {
     individualDigits.pop()
-    calculationDisplayText.textContent = previousCalcDisplayText;
-    // resultContainer.textContent = storedResults.at(-1)
-    // console.log(calculationArray);
-    // console.log(poppedDigits);
-}
-
-function opNum(btn, numberBuilder) {
-    individualDigits = []
-    storedResults.push(tempResult)
-    calculationArray.push(tempResult);
-    tempResult = [];
-    numberBuilder = []
-    console.log(calculationArray);
-
-    calculationDisplayText.textContent += btn.textContent;
-
-
-    // Get operator symbol from calc button
-
-    if (btn.textContent === "-") {
-        activeOperator = subtract
-    } else {
-        activeOperator = add;
-    }
-}
-function plusMinus() {
-    console.log(totalTypedNow);
-
-}
-function regNum(btn) {
-    let digit = btn.textContent
-
-    previousCalcDisplayText = calculationDisplayText.textContent;
-
-    
-    if (digit === ".") {
-        console.log("its a dott");
-        calculationDisplayText.textContent = calculationDisplayText.textContent += digit;
-        decimal = true;
-    } else if (decimal === true) {
-        let decimalDigit = `.${digit}`;
-        individualDigits.push(decimalDigit)
-        calculationDisplayText.textContent = calculationDisplayText.textContent += digit;
+    if (decimal) {
+        calculationDisplayText.textContent = previousCalcDisplayTextBeforeDot;
         decimal = false;
     } else {
-        individualDigits.push(digit)
-        calculationDisplayText.textContent = calculationDisplayText.textContent += digit;
-
+        calculationDisplayText.textContent = previousCalcDisplayText;
     }
-    console.log(individualDigits);
-    // individualDigits.push(btn)
-    // console.log(thisNumber);
-    // thisNumber = 0;
+
+}
+
+function opNum(btn) {
+    lastOperatorSymbol = btn.textContent;
+
+    if (allOperatorSymbols.some(symbol => ` ${symbol} ` === calcDisplayArray[calcDisplayArray.length - 1])) {
+        console.log("its two ops in row");
+        calcDisplayArray[calcDisplayArray.length - 1] = ` ${lastOperatorSymbol} `;
+    } else {
+        calcDisplayArray.push(` ${lastOperatorSymbol} `)
+    }
+    individualDigits = []
+    // calcWholeNumbers.push(parseFloat(joinedDigits))
+    joinedDigits;
+
+    if (btn.textContent === "-") { activeOperator = subtract } else { activeOperator = add; }
+}
+function plusMinus() {
+
+    let currentDigits = individualDigits;
+    console.log(currentDigits);
+
+    if (currentDigits.length === 0) {
+        console.log("its 0");
+        currentDigits.push("-")
+    } else if (currentDigits.length === 1 && currentDigits[0] === "-") {
+        console.log("its 1");
+        currentDigits.shift()
+    } else if (currentDigits.length > 1 && currentDigits[0] === "-") {
+        console.log("its over 1");
+        currentDigits.shift()
+    } else {
+        currentDigits.unshift("-")
+    }
+
+    if (currentDigits.length !== 0) {
+        individualDigits = currentDigits;   
+    }
+
+    joinedDigits = individualDigits.join("")
+
+    if (individualDigits.length < 2 && calcDisplayArray.length >= 2) {
+        calcDisplayArray.push(joinedDigits)
+        calcWholeNumbers.push(parseFloat(joinedDigits))
+    } else {
+        console.log("here");
+        calcDisplayArray[calcDisplayArray.length - 1] = joinedDigits;
+        calcWholeNumbers[calcWholeNumbers.length - 1] = parseFloat(joinedDigits);
+    }
+}
+
+function regNum(btn) {
+    let digit = btn.textContent
+    previousCalcDisplayText = calculationDisplayText.textContent;
 
 
+    individualDigits.push(digit)
+    joinedDigits = individualDigits.join("")
+    if (individualDigits.length < 2) {
+        calcDisplayArray.push(joinedDigits)
+        calcWholeNumbers.push(parseFloat(joinedDigits))
+    } else {
+        console.log("here");
+        calcDisplayArray[calcDisplayArray.length - 1] = joinedDigits;
+        calcWholeNumbers[calcWholeNumbers.length - 1] = parseFloat(joinedDigits);
+    }
 }
 
 function calcLiveResult(totalTypedNow) {
@@ -125,16 +153,19 @@ function displayCalculation(totalTypedNow) {
 
 }
 
-function add(a, b) {return a + b;}
+function add(a, b) { return a + b; }
 
-function subtract(a, b) {return a - b;}
+function subtract(a, b) { return a - b; }
 
-function multiply(a, b) {return a * b;}
+function multiply(a, b) { return a * b; }
 
-function divide(a, b) {return a / b;}
+function divide(a, b) { return a / b; }
 
 // console.log(add(2,-8));
 
 function clearCalc() {
-
+    individualDigits = []
+    joinedDigits;
+    calcDisplayArray = []
+    joinedCalcDisplayArray;
 }
